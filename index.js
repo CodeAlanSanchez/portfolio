669a5154fa27449aa5333a1d4fccb98697fa8244
Alan Sanchez <me@alansanchez.dev>
2023-06-25 19:50:11 -0700

    Revamp whole site styles and add resume download

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-
 let projects = $("div.project");
 
 let buttons = $(".button-filter");
+let buttons2 = $(".technologies li");
 
 let projectsToShow = [];
 
@@ -24,14 +24,20 @@ const filter = (word) => {
     $(".project-list").empty();
     show(projectsToShow);
     projectsToShow = [];
-}
+};
 
 const show = (projectsToShow) => {
-        for (let i = 0; i < projectsToShow.length; i++) {
-            $(".project-list").append(projects[projectsToShow[i]])
-        }
+    for (let i = 0; i < projectsToShow.length; i++) {
+        $(".project-list").append(projects[projectsToShow[i]]);
     }
+};
 
 for (let index = 0; index < buttons.length; index++) {
     buttons[index].addEventListener("click", () => filter(buttons[index].textContent.toLowerCase()));
+}
+
+for (let index = 0; index < buttons2.length; index++) {
+    buttons2[index].addEventListener("click", () => {
+        filter(buttons2[index].textContent.toLowerCase());
+    });
 }
